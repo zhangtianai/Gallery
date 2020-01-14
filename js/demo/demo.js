3e369e2c6355d009001846910c8115b52d8a1f29
zhangtianai <1361006944@qq.com>
2020-01-14 23:39:48 +0800

    Update demo.js

--- js/demo/demo.js
+++ js/demo/demo.js
@@ -65,15 +65,13 @@ $(function() {
     [
       {
         title: 'Sintel',
-        href: 'https://archive.org/download/Sintel/sintel-2048-surround.mp4',
+        href: 'http://13.56.207.99:2015/videodemo/ok.mp4',
         type: 'video/mp4',
         poster: 'https://i.imgur.com/MUSw4Zu.jpg'
       },
       {
         title: 'Big Buck Bunny',
-        href:
-          'https://upload.wikimedia.org/wikipedia/commons/c/c0/' +
-          'Big_Buck_Bunny_4K.webm',
+        href:'http://13.56.207.99:2015/videodemo/ok.mp4',
         type: 'video/webm',
         poster:
           'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/' +
@@ -81,24 +79,11 @@ $(function() {
       },
       {
         title: 'Elephants Dream',
-        href:
-          'https://upload.wikimedia.org/wikipedia/commons/8/83/' +
-          'Elephants_Dream_%28high_quality%29.ogv',
+        href:'http://13.56.207.99:2015/videodemo/ok.mp4',
         type: 'video/ogg',
         poster:
           'https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/' +
           'Elephants_Dream_s1_proog.jpg/800px-Elephants_Dream_s1_proog.jpg'
-      },
-      {
-        title: 'LES TWINS - An Industry Ahead',
-        type: 'text/html',
-        youtube: 'zi4CIXpx7Bg'
-      },
-      {
-        title: 'KN1GHT - Last Moon',
-        type: 'text/html',
-        vimeo: '73686146',
-        poster: 'https://secure-a.vimeocdn.com/ts/448/835/448835699_960.jpg'
       }
     ],
     {
